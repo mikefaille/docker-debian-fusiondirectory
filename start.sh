2bae60eefdce165b1d1998196c98f44433fc6ac4
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-12-15 02:09:31 +0100

    Fix restarts

--- start.sh
+++ start.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-if [ ! -e "/.setup" ]; then
+LDAP_DOMAIN_DC1=`echo $LDAP_DOMAIN|cut -d. -f1`
+LDAP_DOMAIN_DC2=`echo $LDAP_DOMAIN|cut -d. -f2`
+LDAP_DOMAIN_DC3=`echo $LDAP_DOMAIN|cut -d. -f3`
 
-    LDAP_DOMAIN_DC1=`echo $LDAP_DOMAIN|cut -d. -f1`
-    LDAP_DOMAIN_DC2=`echo $LDAP_DOMAIN|cut -d. -f2`
-    LDAP_DOMAIN_DC3=`echo $LDAP_DOMAIN|cut -d. -f3`
+export LDAP_DOMAIN_DC="dc=$LDAP_DOMAIN_DC1"
+if [ _$LDAP_DOMAIN_DC2 != _ ]; then
+    LDAP_DOMAIN_DC="$LDAP_DOMAIN_DC,dc=$LDAP_DOMAIN_DC2"
+fi
+if [ _$LDAP_DOMAIN_DC3 != _ ]; then
+    LDAP_DOMAIN_DC="$LDAP_DOMAIN_DC,dc=$LDAP_DOMAIN_DC3"
+fi
 
-    export LDAP_DOMAIN_DC="dc=$LDAP_DOMAIN_DC1"
-    if [ _$LDAP_DOMAIN_DC2 != _ ]; then
-        LDAP_DOMAIN_DC="$LDAP_DOMAIN_DC,dc=$LDAP_DOMAIN_DC2"
-    fi
-    if [ _$LDAP_DOMAIN_DC3 != _ ]; then
-        LDAP_DOMAIN_DC="$LDAP_DOMAIN_DC,dc=$LDAP_DOMAIN_DC3"
-    fi
+envsubst < /fusiondirectory.conf > /etc/fusiondirectory/fusiondirectory.conf
 
-    envsubst < /fusiondirectory.conf > /etc/fusiondirectory/fusiondirectory.conf
+if [ ! -e "/etc/ldap/fusionready" ]; then
     yes Yes | fusiondirectory-setup --check-config
     fusiondirectory-setup --yes --check-ldap << EOF
 admin
 $FUSIONDIRECTORY_PASSWORD
 $FUSIONDIRECTORY_PASSWORD
 EOF
-    touch /.setup
+    touch /etc/ldap/fusionready
 fi
 
 . /etc/apache2/envvars
